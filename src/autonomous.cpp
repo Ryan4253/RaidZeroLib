7a21897322014a3762325a1c0f437c754f6961ec
PR13ST3360 <59126610+PR13ST3360@users.noreply.github.com>
2021-03-12 00:54:31 +0800

    Delete autonomous.cpp

--- src/autonomous.cpp
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-
-void autonomous() {
-  Odom({0, 0, 90});
-  Robot::startTask("Odometry", Odom::updatePos);
-  Robot::startTask("Display", Robot::displayPosition);
-  pros::delay(100);
-  Drive::moveTo({0, 48}, 1, 200_s);
-}

94e8cf4b0bad155f51442b0fa7b25e074a0924e0
Yessir4253 <67040509+Yessir4253@users.noreply.github.com>
2021-05-24 14:38:08 +0800

    improved spacing

--- src/lib4253/Controller/PurePursuit.cpp
+++ src/lib4253/Controller/PurePursuit.cpp
@@ -2,9 +2,9 @@
 #include "lib4253/Controller/PurePursuit.hpp"
 
 void PurePursuitFollower::initialize(){
-  settled = false;
-  prevClosestPoint = 0; closestPoint = 0;
-  prevLookAheadPoint = 0;
+    settled = false;
+    prevClosestPoint = 0; closestPoint = 0;
+    prevLookAheadPoint = 0;
 }
 
 void PurePursuitFollower::setPath(SimplePath p){
@@ -24,11 +24,11 @@ void PurePursuitFollower::setKinematics(double v, double a){
 }
 
 void PurePursuitFollower::setTrackWidth(double size){
-  trackWidth = size;
+    trackWidth = size;
 }
 
 void PurePursuitFollower::setLookAhead(double dist){
-  radius = dist;
+    radius = dist;
 }
 
 void PurePursuitFollower::generateVelocity(){
@@ -52,8 +52,8 @@ void PurePursuitFollower::calcClosestPoint(Pose2D currentPos){
     for(int i = prevClosestPoint+1; i < path.getSize(); i++){
         double dist = currentPos.distanceTo(path.getWaypoint(i));
         if(dist < minDist){
-          minDist = dist;
-          minIndex = i;
+            minDist = dist;
+            minIndex = i;
         }
     }
 
@@ -63,63 +63,63 @@ void PurePursuitFollower::calcClosestPoint(Pose2D currentPos){
 
 void PurePursuitFollower::calcLookAheadPoint(Pose2D currentPos){
   for(int i = prevLookAheadPoint; i < path.getSize()-1; i++){
-    Point2D start = path.getWaypoint(i);
-    Point2D end = path.getWaypoint(i+1);
+        Point2D start = path.getWaypoint(i);
+        Point2D end = path.getWaypoint(i+1);
 
-    Point2D d = end - start;
-    Point2D f = start - currentPos;
+        Point2D d = end - start;
+        Point2D f = start - currentPos;
 
-    double a = d * d;
-    double b = 2 * (f * d);
-    double c = f * f - radius * radius;
-    double discriminant = b * b - 4 * a * c;
+        double a = d * d;
+        double b = 2 * (f * d);
+        double c = f * f - radius * radius;
+        double discriminant = b * b - 4 * a * c;
 
-    if(discriminant >= 0){
-      discriminant = sqrt(discriminant);
-      double t1 = (-b - discriminant) / (2 * a);
-      double t2 = (-b + discriminant) / (2 * a);
+        if(discriminant >= 0){
+            discriminant = sqrt(discriminant);
+            double t1 = (-b - discriminant) / (2 * a);
+            double t2 = (-b + discriminant) / (2 * a);
 
-      if(t2 >= 0 && t2 <= 0){
-        prevLookAheadPoint = i;
-        lookAheadPoint = start + d * t2;
-        return;
-      }
-      else if(t1 >= 0 && t1 <= 0){
-        prevLookAheadPoint = i;
-        lookAheadPoint = start + d * t1;
-        return;
-      }
+            if(t2 >= 0 && t2 <= 0){
+                prevLookAheadPoint = i;
+                lookAheadPoint = start + d * t2;
+                return;
+            }
+            else if(t1 >= 0 && t1 <= 0){
+                prevLookAheadPoint = i;
+                lookAheadPoint = start + d * t1;
+                return;
+            }   
+        }
     }
-  }
 }
 
 void PurePursuitFollower::calcCurvature(Pose2D currentPos){
-  double a = -tan(currentPos.angle), b = 1, c = tan(currentPos.angle)*currentPos.x - currentPos.y;
-  double x = abs(lookAheadPoint.x * a + lookAheadPoint.y * b + c) / sqrt(a * a + b * b);
-  double side = sin(currentPos.angle) * (lookAheadPoint.x * currentPos.x) - cos(currentPos.angle) * (lookAheadPoint.y - currentPos.y);
-  side /= abs(side);
+    double a = -tan(currentPos.angle), b = 1, c = tan(currentPos.angle)*currentPos.x - currentPos.y;
+    double x = abs(lookAheadPoint.x * a + lookAheadPoint.y * b + c) / sqrt(a * a + b * b);
+    double side = sin(currentPos.angle) * (lookAheadPoint.x * currentPos.x) - cos(currentPos.angle) * (lookAheadPoint.y - currentPos.y);
+    side /= abs(side);
 
-  curvature = (2 * x) / (radius * radius) * side;
+    curvature = (2 * x) / (radius * radius) * side;
 }
 
 std::pair<double, double> PurePursuitFollower::calcPower(Pose2D currentPos){
-  double lTarget = velocity[closestPoint] * (2 + curvature * trackWidth) / 2;
-  double rTarget = velocity[closestPoint] * (2 + curvature * trackWidth) / 2;
+    double lTarget = velocity[closestPoint] * (2 + curvature * trackWidth) / 2;
+    double rTarget = velocity[closestPoint] * (2 + curvature * trackWidth) / 2;
 
-  if(lTarget < 0.2 && rTarget < 0.2){
-    settled = true;
-  }
+    if(lTarget < 0.2 && rTarget < 0.2){
+        settled = true;
+    }
 
-  return std::make_pair(lTarget, rTarget);
-}
+    return std::make_pair(lTarget, rTarget);
+}   
 
 std::pair<double, double> PurePursuitFollower::step(Pose2D currentPos){
-  calcClosestPoint(currentPos);
-  calcLookAheadPoint(currentPos);
-  calcCurvature(currentPos);
-  return calcPower(currentPos);
+    calcClosestPoint(currentPos);
+    calcLookAheadPoint(currentPos);
+    calcCurvature(currentPos);
+    return calcPower(currentPos);
 }
 
 bool PurePursuitFollower::isSettled(){
-  return settled;
+    return settled;
 }
--- src/lib4253/Subsystems/Drive.cpp
+++ src/lib4253/Subsystems/Drive.cpp
@@ -4,341 +4,338 @@
 /* CONSTRUCTOR */
 
 Drive::Drive(const std::initializer_list<okapi::Motor> &l, const std::initializer_list<okapi::Motor> &r):
- left(l), right(r)
+    left(l), right(r)
 {
 }
 
 Drive& Drive::withOdometry(CustomOdometry* tracker){
-  odom = tracker;
-  return *this;
+    odom = tracker;
+    return *this;
 }
 
 Drive& Drive::withDimensions(std::tuple<double> wheel, std::tuple<double, double> gear, std::tuple<double> track){
-  wheelSize = std::get<0>(wheel);
-  gearRatio = std::get<0>(gear) / std::get<1>(gear);
-  trackWidth = std::get<0>(track);
+    wheelSize = std::get<0>(wheel);
+    gearRatio = std::get<0>(gear) / std::get<1>(gear);
+    trackWidth = std::get<0>(track);
 
-  PPTenshi.setTrackWidth(trackWidth);
-  return *this;
+    PPTenshi.setTrackWidth(trackWidth);
+    return *this;
 }
 
 Drive& Drive::withDrivePID(std::tuple<double, double, double> gain, std::tuple<double, double> IGain, std::tuple<double> emaGain){
-  drivePID.setGain(std::get<0>(gain), std::get<1>(gain), std::get<2>(gain));
-  drivePID.setIGain(std::get<0>(IGain), std::get<1>(IGain));
-  drivePID.setEMAGain(std::get<0>(emaGain));
-  return *this;
+    drivePID.setGain(std::get<0>(gain), std::get<1>(gain), std::get<2>(gain));
+    drivePID.setIGain(std::get<0>(IGain), std::get<1>(IGain));
+    drivePID.setEMAGain(std::get<0>(emaGain));
+    return *this;
 }
 
 Drive& Drive::withTurnPID(std::tuple<double, double, double> gain, std::tuple<double, double> IGain, std::tuple<double> emaGain){
-  turnPID.setGain(std::get<0>(gain), std::get<1>(gain), std::get<2>(gain));
-  turnPID.setIGain(std::get<0>(IGain), std::get<1>(IGain));
-  turnPID.setEMAGain(std::get<0>(emaGain));
-  return *this;
+    turnPID.setGain(std::get<0>(gain), std::get<1>(gain), std::get<2>(gain));
+    turnPID.setIGain(std::get<0>(IGain), std::get<1>(IGain));
+    turnPID.setEMAGain(std::get<0>(emaGain));
+    return *this;
 }
 
 Drive& Drive::withPurePursuit(std::tuple<double> lookAhead, std::tuple<double> turnGain, std::tuple<double, double> kinematics){
-  PPTenshi.setLookAhead(std::get<0>(lookAhead));
-  PPTenshi.setTurnGain(std::get<0>(turnGain));
-  PPTenshi.setKinematics(std::get<0>(kinematics), std::get<1>(kinematics));
-  return *this;
+    PPTenshi.setLookAhead(std::get<0>(lookAhead));
+    PPTenshi.setTurnGain(std::get<0>(turnGain));
+    PPTenshi.setKinematics(std::get<0>(kinematics), std::get<1>(kinematics));
+    return *this;
 }
 
 Drive& Drive::withSlew(int acc, int dec){
-  driveSlew.setStep(acc, dec);
-  return *this;
+    driveSlew.setStep(acc, dec);
+    return *this;
 }
 
 Drive& Drive::withVelocityFeedForward(std::tuple<double, double, double> l, std::tuple<double, double, double> r){
-  leftVelController.setGain(std::get<0>(l), std::get<1>(l), std::get<2>(l));
-  rightVelController.setGain(std::get<0>(r), std::get<1>(r), std::get<2>(r));
-  return *this;
+    leftVelController.setGain(std::get<0>(l), std::get<1>(l), std::get<2>(l));
+    rightVelController.setGain(std::get<0>(r), std::get<1>(r), std::get<2>(r));
+    return *this;
 }
 
 void Drive::initialize(){
-  Robot::setBrakeMode(left, COAST);
-  Robot::setBrakeMode(right, COAST);
-  resetIMU();
+    Robot::setBrakeMode(left, COAST);
+    Robot::setBrakeMode(right, COAST);
+    resetIMU();
 }
 
 /* SENSOR FUNCTION */
 
 void Drive::resetIMU(){
-  imuTop.reset();
-  imuBottom.reset();
-  pros::delay(2000+100);
+    imuTop.reset();
+    imuBottom.reset();
+    pros::delay(2000+100);
 }
 
 double Drive::getAngle() {
-  return (imuTop.get_rotation() + imuBottom.get_rotation()) / 2;
+    return (imuTop.get_rotation() + imuBottom.get_rotation()) / 2;
 }
 
 /* STATE FUNCTION */
 
 Drive::State Drive::getState(){
-  return driveState;
+    return driveState;
 }
 
 void Drive::setState(State s){
-  driveState = s;
+    driveState = s;
 }
 
 void Drive::updateState(){
-  int AState = master.getDigital(okapi::ControllerDigital::A);
+    int AState = master.getDigital(okapi::ControllerDigital::A);
 
-  if(AState && !prevAState){
-    if(driveState == TANK){
-      driveState = ARCADE;
+    if(AState && !prevAState){
+        if(driveState == TANK){
+            driveState = ARCADE;
+        }
+        else{
+            driveState = TANK;
+        }
     }
-    else{
-      driveState = TANK;
-    }
-  }
 
-  prevAState = AState;
+    prevAState = AState;
 }
 
 /* TASK FUNCTION */
 
 void Drive::tank(){
-  //std::cout<<"TANK" << std::endl;
+    //std::cout<<"TANK" << std::endl;
 
-  double leftPower = master.getAnalog(okapi::ControllerAnalog::leftY)*127;
-  double rightPower = master.getAnalog(okapi::ControllerAnalog::rightY)*127;
+    double leftPower = master.getAnalog(okapi::ControllerAnalog::leftY)*127;
+    double rightPower = master.getAnalog(okapi::ControllerAnalog::rightY)*127;
 
-  Robot::setPower(left, leftPower);
-  Robot::setPower(right, rightPower);
+    Robot::setPower(left, leftPower);
+    Robot::setPower(right, rightPower);
 }
 
 void Drive::arcade(){
-  //std::cout << "ARCADE" << std::endl;
+    //std::cout << "ARCADE" << std::endl;
 
-  int power = master.getAnalog(okapi::ControllerAnalog::leftY)*127;
-  int turn = master.getAnalog(okapi::ControllerAnalog::rightX)*-127;
-  Point2D finalPower = scaleSpeed(power, turn, 1);
+    int power = master.getAnalog(okapi::ControllerAnalog::leftY)*127;
+    int turn = master.getAnalog(okapi::ControllerAnalog::rightX)*-127;
+    Point2D finalPower = scaleSpeed(power, turn, 1);
 
-  Robot::setPower(left, finalPower.x);
-  Robot::setPower(right, finalPower.y);
+    Robot::setPower(left, finalPower.x);
+    Robot::setPower(right, finalPower.y);
 }
 
 void Drive::run(){
-  while(true){
-    updateState();
+    while(true){
+        updateState();
 
-    switch(driveState){
-      case TANK:
-        tank();
-        break;
+        switch(driveState){
+            case TANK:
+            tank();
+            break;
 
-      case ARCADE:
-        arcade();
-        break;
+            case ARCADE:
+            arcade();
+            break;
+        }
+        pros::delay(3);
     }
-
-    pros::delay(3);
-  }
 }
 
 void Drive::driveTask(void* ptr){
-  pros::delay(10);
-  Drive* that = static_cast<Drive*>(ptr);
-  that->run();
+    pros::delay(10);
+    Drive* that = static_cast<Drive*>(ptr);
+    that->run();
 }
 
 /* AUTON FUNCTIONS */
 
 Point2D Drive::scaleSpeed(double drivePower, double turnPower, double turnScale){
-  double leftPower = drivePower - turnPower * turnScale;
-  double rightPower = drivePower + turnPower * turnScale;
+    double leftPower = drivePower - turnPower * turnScale;
+    double rightPower = drivePower + turnPower * turnScale;
 
-  double maxPower = fmax(std::fabs(leftPower), std::fabs(rightPower));
-  double adjustment = driveSlew.step(maxPower);
-  leftPower = leftPower / maxPower * adjustment;
-  rightPower = rightPower / maxPower * adjustment;
+    double maxPower = fmax(std::fabs(leftPower), std::fabs(rightPower));
+    double adjustment = driveSlew.step(maxPower);
+    leftPower = leftPower / maxPower * adjustment;
+    rightPower = rightPower / maxPower * adjustment;
 
-  return {leftPower, rightPower};
+    return {leftPower, rightPower};
 }
 
 void Drive::moveDistanceLMP(double distance){
-  okapi::Timer timer;
-  bruhMobile->setDistance(distance);
-  double initTime = timer.millis().convert(okapi::second), timeElapsed;
-  double initAngle = odom->getAngleDeg();
-  do{
-    timeElapsed = timer.millis().convert(okapi::second) - initTime;
-    double velocity = bruhMobile->getVelocityTime(timeElapsed);
-    double angle = odom->getAngleDeg() - initAngle;
+    okapi::Timer timer;
+    bruhMobile->setDistance(distance);
+    double initTime = timer.millis().convert(okapi::second), timeElapsed;
+    double initAngle = odom->getAngleDeg();
+    do {
+        timeElapsed = timer.millis().convert(okapi::second) - initTime;
+        double velocity = bruhMobile->getVelocityTime(timeElapsed);
+        double angle = odom->getAngleDeg() - initAngle;
 
-    double rpm = Math::linearVelToRPM(velocity, gearRatio, wheelSize/2);
-    left.moveVelocity(rpm + angle * 5);
-    right.moveVelocity(rpm - angle * 5);
+        double rpm = Math::linearVelToRPM(velocity, gearRatio, wheelSize/2);
+        left.moveVelocity(rpm + angle * 5);
+        right.moveVelocity(rpm - angle * 5);
 
-  }while(timeElapsed <= bruhMobile->getTotalTime());
+    } while(timeElapsed <= bruhMobile->getTotalTime());
 
-  Robot::setPower(left, 0);
-  Robot::setPower(right, 0);
-}
+    Robot::setPower(left, 0);
+    Robot::setPower(right, 0);
+}   
 
 void Drive::moveDistanceLMPD(double dist){
 
-  double initLeft = odom->getEncoderLeft();
-  double initRight = odom->getEncoderRight();
-  double initAngle = odom->getAngleDeg();
-  double totalDist;
+    double initLeft = odom->getEncoderLeft();
+    double initRight = odom->getEncoderRight();
+    double initAngle = odom->getAngleDeg();
+    double totalDist;
 
-  do{
-    double leftDist = odom->getEncoderLeft() - initLeft;
-    double rightDist = odom->getEncoderRight() - initRight;
-    totalDist = (leftDist + rightDist) / 2;
-    double angle = odom->getAngleDeg() - initAngle;
+    do {
+        double leftDist = odom->getEncoderLeft() - initLeft;
+        double rightDist = odom->getEncoderRight() - initRight;
+        totalDist = (leftDist + rightDist) / 2;
+        double angle = odom->getAngleDeg() - initAngle;
 
-    double velocity = bruhMobile->getVelocityDist(totalDist);
-    double rpm = Math::linearVelToRPM(velocity, gearRatio, wheelSize/2);
-    left.moveVelocity(rpm + angle * 5);
-    right.moveVelocity(rpm - angle * 5);
+        double velocity = bruhMobile->getVelocityDist(totalDist);
+        double rpm = Math::linearVelToRPM(velocity, gearRatio, wheelSize/2);
+        left.moveVelocity(rpm + angle * 5);
+        right.moveVelocity(rpm - angle * 5);
 
-  }while(totalDist < dist);
+    } while(totalDist < dist);
 }
 
 void Drive::followPath(SimplePath path){
-  PPTenshi.setPath(path);
+    PPTenshi.setPath(path);
 
-  do{
-    Pose2D currentPos = odom->getPos();
-    std::pair<double, double> velocity = PPTenshi.step(currentPos);
-    double leftVel = Math::linearVelToRPM(velocity.first, gearRatio, wheelSize/2);
-    double rightVel = Math::linearVelToRPM(velocity.second, gearRatio, wheelSize/2);
+    do {
+        Pose2D currentPos = odom->getPos();
+        std::pair<double, double> velocity = PPTenshi.step(currentPos);
+        double leftVel = Math::linearVelToRPM(velocity.first, gearRatio, wheelSize/2);
+        double rightVel = Math::linearVelToRPM(velocity.second, gearRatio, wheelSize/2);
 
-    left.moveVelocity(leftVel);
-    right.moveVelocity(rightVel);
-  }while(!PPTenshi.isSettled());
+        left.moveVelocity(leftVel);
+        right.moveVelocity(rightVel);
+    } while(!PPTenshi.isSettled());
 
-  Robot::setPower(left, 0);
-  Robot::setPower(right, 0);
-}
+    Robot::setPower(left, 0);
+    Robot::setPower(right, 0);
+}   
 
 void Drive::followPath(std::string name){
-  Trajectory path = Robot::getPath(name);
-  int n = path.getSize();
-  EmaFilter velFilter(0.65);
-  velFilter.reset();
+    Trajectory path = Robot::getPath(name);
+    int n = path.getSize();
+    EmaFilter velFilter(0.65);
+    velFilter.reset();
 
-  for(int i = 0; i < n; i++){
-    double leftActualVel = velFilter.filter(left.getActualVelocity());
-    std::cout << i-1 << " " << leftActualVel << std::endl;
-    std::pair<TrajectoryPoint, TrajectoryPoint> kin = path.getKinematics(i);
-    double leftRPM = kin.first.velocity * 12 * 60 / (2 * M_PI) / 2 * 7 / 3;
-    std::cout << i << " " << leftRPM << " ";
+    for(int i = 0; i < n; i++){
+        double leftActualVel = velFilter.filter(left.getActualVelocity());
+        std::cout << i-1 << " " << leftActualVel << std::endl;
+        std::pair<TrajectoryPoint, TrajectoryPoint> kin = path.getKinematics(i);
+        double leftRPM = kin.first.velocity * 12 * 60 / (2 * M_PI) / 2 * 7 / 3;
+        std::cout << i << " " << leftRPM << " ";
 
-    /*
-  	double rightRPM = kin.second.velocity * 12 * 60 / (2 * M_PI) / 2 * 7 / 3;
+        /*
+        	double rightRPM = kin.second.velocity * 12 * 60 / (2 * M_PI) / 2 * 7 / 3;
 
-    std::cout << i << " " << leftRPM << " ";
-    //std::cout << leftRPM << " " << rightRPM << std::endl;
+        std::cout << i << " " << leftRPM << " ";
+        //std::cout << leftRPM << " " << rightRPM << std::endl;
 
-    left.moveVelocity(leftRPM);
-    right.moveVelocity(rightRPM);
-    */
-    double l = leftVelController.calcPower(kin.first, left.getActualVelocity());
-    double r = rightVelController.calcPower(kin.second, right.getActualVelocity());
-    Robot::setPower(left, l);
-    Robot::setPower(right, r);
+        left.moveVelocity(leftRPM);
+        right.moveVelocity(rightRPM);
+        */
+        double l = leftVelController.calcPower(kin.first, left.getActualVelocity());
+        double r = rightVelController.calcPower(kin.second, right.getActualVelocity());
+        Robot::setPower(left, l);
+        Robot::setPower(right, r);
 
-  	pros::delay(10);
-  }
-  left.moveVoltage(0);
-  right.moveVoltage(0);
+        pros::delay(10);
+    }
+    left.moveVoltage(0);
+    right.moveVoltage(0);
 
 }
 
 
 void Drive::moveDistance(double dist, okapi::QTime timeLimit) {
-  Pose2D currentPos = odom->getPos();
-  Point2D displacement = {dist * cos(currentPos.angle), dist * sin(currentPos.angle)};
-  Point2D target = currentPos + displacement;
-  moveTo(target, 1, timeLimit);
+    Pose2D currentPos = odom->getPos();
+    Point2D displacement = {dist * cos(currentPos.angle), dist * sin(currentPos.angle)};
+    Point2D target = currentPos + displacement;
+    moveTo(target, 1, timeLimit);
 }
 
 void Drive::moveTo(Point2D target, double turnScale, okapi::QTime timeLimit){
-  drivePID.initialize();
-  turnPID.initialize();
-  driveSlew.reset();
-  okapi::Timer timer = okapi::Timer();
-  double distToTarget; okapi::QTime startTime = timer.millis();
+    drivePID.initialize();
+    turnPID.initialize();
+    driveSlew.reset();
+    okapi::Timer timer = okapi::Timer();
+    double distToTarget; okapi::QTime startTime = timer.millis();
 
-  do{
-    Pose2D currentPos = odom->getPos();
-    Point2D closestPoint = currentPos.closest(target);
-    pros::lcd::print(0, "CURRENT X: %lf", (double)currentPos.x);
-    pros::lcd::print(1, "CURRENT Y: %lf", (double)currentPos.y);
-    pros::lcd::print(2, "CURRENT A: %lf", (double)currentPos.angle);
+    do {
+        Pose2D currentPos = odom->getPos();
+        Point2D closestPoint = currentPos.closest(target);
+        pros::lcd::print(0, "CURRENT X: %lf", (double)currentPos.x);
+        pros::lcd::print(1, "CURRENT Y: %lf", (double)currentPos.y);
+        pros::lcd::print(2, "CURRENT A: %lf", (double)currentPos.angle);
 
-    distToTarget = currentPos.distanceTo(target);
-    double angleToTarget = currentPos.angleTo(target);
-    double distToClose = currentPos.distanceTo(closestPoint);
-    double angleToClose = currentPos.angleTo(closestPoint);
+        distToTarget = currentPos.distanceTo(target);
+        double angleToTarget = currentPos.angleTo(target);
+        double distToClose = currentPos.distanceTo(closestPoint);
+        double angleToClose = currentPos.angleTo(closestPoint);
 
-    double driveError = (std::fabs(angleToClose) >= 90) ? -distToClose : distToClose;
-    double turnError = (std::fabs(distToTarget) < 5)  ? 0 : Math::wrapAngle90(angleToTarget);
+        double driveError = (std::fabs(angleToClose) >= 90) ? -distToClose : distToClose;
+        double turnError = (std::fabs(distToTarget) < 5)  ? 0 : Math::wrapAngle90(angleToTarget);
 
-    pros::lcd::print(4, "Drive Error: %lf", driveError);
-    pros::lcd::print(5, "Turn Error: %lf", turnError);
+        pros::lcd::print(4, "Drive Error: %lf", driveError);
+        pros::lcd::print(5, "Turn Error: %lf", turnError);
 
-    double drivePower = drivePID.update(-driveError);
-    double turnPower = turnPID.update(-turnError);
+        double drivePower = drivePID.update(-driveError);
+        double turnPower = turnPID.update(-turnError);
 
-    pros::lcd::print(6, "Drive Power: %lf", drivePower);
-    pros::lcd::print(7, "Turn Power: %lf", turnPower);
-    Point2D driveSpeed = Drive::scaleSpeed(drivePower, turnPower, 0.3);
+        pros::lcd::print(6, "Drive Power: %lf", drivePower);
+        pros::lcd::print(7, "Turn Power: %lf", turnPower);
+        Point2D driveSpeed = Drive::scaleSpeed(drivePower, turnPower, 0.3);
 
-    Robot::setPower(left, driveSpeed.x);
-    Robot::setPower(right, driveSpeed.y);
-    pros::delay(10);
-
-
-  }while((distToTarget >= 0.125) && timeLimit > (timer.millis()-startTime));
-  Robot::setPower(left, 0);
-  Robot::setPower(right, 0);
+        Robot::setPower(left, driveSpeed.x);
+        Robot::setPower(right, driveSpeed.y);
+        pros::delay(10);
+    } while((distToTarget >= 0.125) && timeLimit > (timer.millis()-startTime));
+    Robot::setPower(left, 0);
+    Robot::setPower(right, 0);
 }
 
 void Drive::turnAngle(double angle, okapi::QTime timeLimit){
-  turnPID.initialize();
-  driveSlew.reset();
-  double initAngle = odom->getAngleDeg(), error, power;
-  angle = Math::wrapAngle180(angle);
-  okapi::Timer timer = okapi::Timer();
-  okapi::QTime startTime = timer.millis();
+    turnPID.initialize();
+    driveSlew.reset();
+    double initAngle = odom->getAngleDeg(), error, power;
+    angle = Math::wrapAngle180(angle);
+    okapi::Timer timer = okapi::Timer();
+    okapi::QTime startTime = timer.millis();
 
 
-  do{
-    error = (angle - (odom->getAngleDeg()-initAngle));
-    power = turnPID.update(error);
-    power = driveSlew.step(power);
+    do {
+        error = (angle - (odom->getAngleDeg()-initAngle));
+        power = turnPID.update(error);
+        power = driveSlew.step(power);
 
-    Robot::setPower(left, power);
-    Robot::setPower(right, -power);
-    pros::delay(10);
-  }while((error >= 2) && timeLimit > (timer.millis()-startTime));
+        Robot::setPower(left, power);
+        Robot::setPower(right, -power);
+        pros::delay(10);
+    } while((error >= 2) && timeLimit > (timer.millis()-startTime));
 
-  Robot::setPower(left, 0);
-  Robot::setPower(right, 0);
+    Robot::setPower(left, 0);
+    Robot::setPower(right, 0);
 }
 
 void Drive::turnToAngle(double angle, okapi::QTime timeLimit){
-  turnPID.initialize();
-  driveSlew.reset();
-  angle = Math::wrapAngle180(angle); double error, power;
-  okapi::Timer timer = okapi::Timer(); okapi::QTime startTime = timer.millis();
+    turnPID.initialize();
+    driveSlew.reset();
+    angle = Math::wrapAngle180(angle); double error, power;
+    okapi::Timer timer = okapi::Timer(); okapi::QTime startTime = timer.millis();
 
-  do{
-    error = Math::wrapAngle180(angle - (odom->getAngleDeg()));
-    power = turnPID.update(error);
-    power = driveSlew.step(power);
+    do {
+        error = Math::wrapAngle180(angle - (odom->getAngleDeg()));
+        power = turnPID.update(error);
+        power = driveSlew.step(power);
 
-    Robot::setPower(left, power);
-    Robot::setPower(right, -power);
-    pros::delay(10);
-  }while((error >= 2) && timeLimit > (timer.millis()-startTime));
+        Robot::setPower(left, power);
+        Robot::setPower(right, -power);
+        pros::delay(10);
+    } while((error >= 2) && timeLimit > (timer.millis()-startTime));
 
-  Robot::setPower(left, 0);
-  Robot::setPower(right, 0);
+    Robot::setPower(left, 0);
+    Robot::setPower(right, 0);
 }

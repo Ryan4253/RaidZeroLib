06a398c99a3e6035c17e417c09ae8014cd8f49f1
Ryan <ryan.liao0305@gmail.com>
2023-11-11 17:58:13 -0500

    Got navbar working

--- scripts/doxygen.sh
+++ scripts/doxygen.sh
@@ -1,3 +1,3 @@
 cd -
 rm -rf docs/html docs/latex docs/xml
-./docs/m.css/documentation/doxygen.py docs/Doxyfile-mcss --debug --index-pages docs/index.md
+./docs/m.css/documentation/doxygen.py --debug docs/conf.py

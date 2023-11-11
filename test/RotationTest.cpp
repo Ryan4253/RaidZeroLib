4b76bcaf3aa8e40d116f05f388eaeaf536f6b13a
Ryan <ryan.liao0305@gmail.com>
2023-11-11 01:59:00 -0500

    Add codecov.yml,  edit cmakelist

--- test/RotationTest.cpp
+++ test/RotationTest.cpp
@@ -9,6 +9,4 @@ enum State { OPEN = 0, CLOSE = 1, IDLE = 2 };
 TEST(RotationTest, constructor) {
     rz::Rotation angle(3_ft, 3_ft);
     ASSERT_EQ(angle.Theta().convert(radian), ((1_pi) / 4));
-    rz::StateMachine<State> wassup;
-    ASSERT_EQ(wassup.getState(), State::IDLE);
 }
